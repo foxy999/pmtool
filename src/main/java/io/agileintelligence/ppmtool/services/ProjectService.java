0c16b4754e52d1b432900702d10de4fd5621549b
kistune999 <fox-inflames@outlook.com>
2020-02-07 11:52:42 +0100

    added delete project

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -12,32 +12,37 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project)
-    {
-        try{
+    public Project saveOrUpdateProject(Project project) {
+        try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
 
-        }catch(Exception e)
-        {
-            throw new ProjectIdException("Project ID '" +project.getProjectIdentifier().toUpperCase()+"' already exists");
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
 
     }
 
 
-    public Project findProjectByIdentifier(String projectId)
-    {
+    public Project findProjectByIdentifier(String projectId) {
         Project project = projectRepository.findByProjectIdentifier(projectId);
 
-        if(project ==null)
-        {
-            throw new ProjectIdException("Project ID '" +projectId+"' does not exists");
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
         }
         return project;
     }
 
-    public Iterable<Project> findAllProjects(){
+    public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists, can't delete");
+        }
+
+        projectRepository.delete(project);
     }
 }
